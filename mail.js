2ef46a1e048bda1b52d8d5902745ea933cfaf678
Yashi <yashikaranwal03@gmail.com>
2023-12-18 12:25:59 +0530

    update

---
+++ mail.js
@@ -0,0 +1,8 @@
+document.getElementById("send").addEventListener("click",send_func)
+function send_func(){
+    alert("The mail has been sent")
+}
+document.getElementById("cancel").addEventListener("click",cancel_func)
+function cancel_func(){
+    alert("You have canceled the mail")
+}
